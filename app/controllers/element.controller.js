21d85203fb1cbfce4ffd4a7db286621cfcc2f4a9
Felipe Domingues <felipe@doctorwebbrasil.com>
2019-09-01 08:44:05 -0300

    port = 4000

--- app/controllers/element.controller.js
+++ app/controllers/element.controller.js
@@ -9,14 +9,14 @@ exports.create = (req, res) => {
         });
     }
 
-    // Create a Note
+    // Create a Element
     const element= new Element({
         name: req.body.name, 
         number: req.body.number,
         student: req.body.student
     });
 
-    // Save Note in the database
+    // Save element in the database
     element.save()
     .then(data => {
         res.send(data);
@@ -27,46 +27,75 @@ exports.create = (req, res) => {
     });
 };
 
-// Retrieve and return all notes from the database.
-exports.findAll = (req, res) => {
+// Retrieve and return all elements from the database.
+exports.findAll = (res) => {
     Element.find()
     .then(elements => {
         res.send(elements);
     }).catch(err => {
         res.status(500).send({
-            message: err.message || "Some error occurred while retrieving notes."
+            message: err.message || "Some error occurred while retrieving elements."
         });
     });
 };
 
-// Find a single note with a noteId
+// Find a single element with a Id
 exports.findOne = (req, res) => {
-    Element.findById(req.params.noteId)
+    Element.findById(req.params.id)
     .then(element => {
         if(!element) {
             return res.status(404).send({
-                message: "element not found with id " + req.params.elementId
+                message: "element not found with id " + req.params.id
             });            
         }
         res.send(element);
     }).catch(err => {
         if(err.kind === 'ObjectId') {
             return res.status(404).send({
-                message: "Element not found with id " + req.params.elementId
+                message: "Element not found with id " + req.params.id
             });                
         }
         return res.status(500).send({
-            message: "Error retrieving element with id " + req.params.elementId
+            message: "Error retrieving element with id " + req.params.id
         });
     });
 };
 
-// Update a note identified by the noteId in the request
+// Update a element identified by the id in the request
 exports.update = (req, res) => {
+    // Validate Request
+    if(!req.body.id) {
+        return res.status(400).send({
+            message: "Element id can exist"
+        });
+    }
 
+    // Find element and update it with the request body
+    Element.findByIdAndUpdate(req.params.id, {
+        name: req.body.name ,
+        number: req.body.number,
+        student: req.body.student
+    })
+    .then(element => {
+        if(!element) {
+            return res.status(404).send({
+                message: "element not found with id " + req.params.id
+            });
+        }
+        res.send(element);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Element not found with id " + req.params.id
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating element with id " + req.params.id
+        });
+    });
 };
 
-// Delete a note with the specified noteId in the request
+// Delete a element with the specified id in the request
 exports.delete = (req, res) => {
 
 };
